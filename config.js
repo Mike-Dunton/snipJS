04158439339e8fad03913110f75ad43118160519
Mike <dunton@cs.fsu.edu>
2013-12-18 04:12:58 +0400

    Fixed some syntax issues

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
-var config = {}
+var config = {};
 
-config.db = {}
+config.db = {};
 
-config.db.user = 'username';
-config.db.password = 'password';
-config.db.host = 'hostname';
-config.db.port = '1234';
+
 config.db.name = 'snip.db';
-config.db.createTags = 'CREATE TABLE tags (id INTEGER PRIMARY KEY ASC, tag TEXT)\;';
-config.db.createSnip = 'CREATE TABLE snips (id INTEGER PRIMARY KEY ASC, snip TEXT)\;';
-config.db.createRelation = 'CREATE TABLE tagStore (tag_uno INTEGER REFERENCES tags(id),snip_uno INTEGER REFERENCES snips(id))\;';
+config.db.createTags = 'CREATE TABLE tags (id INTEGER PRIMARY KEY ASC, tag TEXT);';
+config.db.createSnip = 'CREATE TABLE snips (id INTEGER PRIMARY KEY ASC, snip TEXT);';
+config.db.createRelation = 'CREATE TABLE tagStore (tag_uno INTEGER REFERENCES tags(id),snip_uno INTEGER REFERENCES snips(id));';
 
 module.exports = config;
